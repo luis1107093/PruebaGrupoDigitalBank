b04bad95610ae3c46c983c071454eb2992418041
Luis Ocampo <Luis Ocampo@DESKTOP-HS3ETIO>
2023-04-12 09:57:49 -0500

    Creacion de vistas y Da

---
+++ Web/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models.DataAnnotations;
+
+namespace Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("UsuarioConsulta");
+        }
+
+        public IActionResult Usuario()
+        {
+            return View();
+        }
+
+        public IActionResult CrearUsuario(UsuarioDA user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Usuario");
+            }
+            return View();
+        }
+    }
+}
---
+++ Web/Models/DataAnnotations/UsuarioDA.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models.DataAnnotations
+{
+    public class UsuarioDA
+    {
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "El nombre debe tener entre 1 y 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimiento es requerida")]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "El sexo es requerido")]
+        public string Sexo { get; set; }
+    }
+}
